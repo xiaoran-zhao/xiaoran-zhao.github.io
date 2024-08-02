5751c602265564c00f8c4ed94ba33f0c83520cc3
XiaoranZhao <405455197@qq.com>
2024-08-02 23:35:48 +0800

    1

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['Teaching', 'Award', 'home', 'author', 'news', 'Fundings', 'Patents', 'ProfessionalActivities', 'GuestEditor', 'ResearchProjects', 'publications', 'ProfessionalService', 'awards', 'GraduatedStudents', 'Research']
+const section_names = ['Students', 'Teaching', 'Award', 'home', 'author', 'news', 'Fundings', 'Patents', 'ProfessionalActivities', 'GuestEditor', 'ResearchProjects', 'publications', 'ProfessionalService', 'awards', 'GraduatedStudents', 'Research']
 
 
 window.addEventListener('DOMContentLoaded', event => {
